853602a6771a46a0521f7e564a15bd674248fd49
OmidDeiminiyat <142876309+OmidDeiminiyat@users.noreply.github.com>
2024-10-14 16:13:21 +0200

    r

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,8 @@
 
-
-
 const fetchData = async () => {
     try {
         const response = await fetch('https://kandapp.netlify.app/api/data');
         const data = await response.json();
-       
         
         document.getElementById('Container').innerHTML = data[1].id; 
     } catch (error) {
